9bd5a2e635ae32bc43a7bc091251b6550eb5a395
Pham Hoang Bao Minh <phamhoangbaominh2005@gmail.com>
2026-07-14 23:02:26 +0700

    Remove max length constraint on Booking Notes

--- LunaWash.DAL/Migrations/20260714155453_IncreaseBookingNotesLength.Designer.cs
+++ LunaWash.DAL/Migrations/20260714155453_IncreaseBookingNotesLength.Designer.cs
@@ -4,6 +4,7 @@ using LunaWash.DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LunaWash.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260714155453_IncreaseBookingNotesLength")]
+    partial class IncreaseBookingNotesLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -124,7 +127,8 @@ namespace LunaWash.DAL.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("Notes")
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(500)
+                        .HasColumnType("nvarchar(500)");
 
                     b.Property<int>("PriorityScore")
                         .HasColumnType("int");
